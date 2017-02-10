d030783ae9793be1dd31676512955de458756f79
Nuno <nunochaves@sapo.pt>
2017-02-10 13:19:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,193 @@
+
+# jupitern/FileParser
+#### PHP File Parser.
+
+read, filter, parse and format {csv, tsv, dsv, variable-length-delimited} and other txt files
+
+## Requirements
+
+PHP 5.4 or higher.
+
+## Installation
+
+Include jupitern/FileParser in your project, by adding it to your composer.json file.
+```javascript
+{
+    "require": {
+        "jupitern/FileParser": "1.*"
+    }
+}
+```
+
+## Usage
+```php
+
+Given a csv file "file.txt" with contents:
+crocodile,reptile,4
+dolphin,mammal,0
+duck,bird,2
+koala,mammal,4
+
+lets:
+    - convert encofing to ISO-9959-1
+    - convert to objects
+    - remove animals with count 0
+    - group by type
+
+// read a file to array
+$objectsArr = \Lib\Parser\Parser::instance()
+    ->setFile("D:\\www\\csv.txt", ',')
+    ->setEncoding('ISO-8859-1', 'UTF-8')
+    ->toObject(['animal', 'type', 'number'])
+    ->filter(function ($line) {
+        return $line->number > 0;
+    })
+    ->group(function ($line) {
+        return $line->type;
+    })
+    ->parse();
+
+echo '<pre>';
+print_r($objectsArr);
+
+/*
+output:
+Array
+(
+    [reptile] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [animal] => crocodile
+                    [type] => reptile
+                    [number] => 4
+
+                )
+
+        )
+
+    [bird] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [animal] => duck
+                    [type] => bird
+                    [number] => 2
+
+                )
+
+        )
+
+    [mammal] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [animal] => koala
+                    [type] => mammal
+                    [number] => 4
+
+                )
+
+        )
+
+    [fish] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [animal] => áéíóú
+                    [type] => fish
+                    [number] => 3
+                )
+
+        )
+
+)
+*/
+
+
+lets:
+   - convert encofing to ISO-9959-1
+   - remove animals with count 0
+   - group by type
+
+$objectsArr = \Lib\Parser\Parser::instance()
+    ->setFile("D:\\www\\csv.txt", ',')
+    ->setEncoding('ISO-8859-1', 'UTF-8')
+    ->filter(function ($line) {
+        return $line[2] > 0;
+    })
+    ->group(function ($line) {
+        return $line[1];
+    })
+    ->parse();
+
+/*
+Output:
+Array
+(
+    [reptile] => Array
+        (
+            [0] => Array
+                (
+                    [0] => crocodile
+                    [1] => reptile
+                    [2] => 4
+
+                )
+
+        )
+
+    [bird] => Array
+        (
+            [0] => Array
+                (
+                    [0] => duck
+                    [1] => bird
+                    [2] => 2
+
+                )
+
+        )
+
+    [mammal] => Array
+        (
+            [0] => Array
+                (
+                    [0] => koala
+                    [1] => mammal
+                    [2] => 4
+
+                )
+
+        )
+
+    [fish] => Array
+        (
+            [0] => Array
+                (
+                    [0] => áéíóú
+                    [1] => fish
+                    [2] => 3
+                )
+
+        )
+
+)
+*/
+
+
+```
+
+## ChangeLog
+
+
+
+## Contributing
+
+ - welcome to discuss a bugs, features and ideas.
+
+## License
+
+jupitern/FileParser is release under the MIT license.
+
+You are free to use, modify and distribute this software
---
+++ src/FileParser.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Jupitern\FileParser;
+
+/*
+ * File Parser
+ * read, filter, parse and format {csv, tsv, dsv, variable-length-delimited} and other txt files
+ *
+ * Author: Nuno Chaves <nunochaves@sapo.pt>
+ * */
+
+class Parser {
+
+    private $filePath = null;
+    private $objectFields = null;
+    private $formatters = [];
+    private $filter = null;
+    private $each = null;
+    private $group = null;
+    private $fromEncoding = null;
+    private $toEncoding = null;
+    private $delimiter;
+
+
+    /**
+     * @return static
+     */
+    public static function instance()
+    {
+        return new static();
+    }
+
+
+    /**
+     * set file to be parsed
+     *
+     * @param $filePath
+     * @param string $delimiter
+     * @return $this
+     */
+    public function setFile($filePath, $delimiter = null)
+    {
+        $this->filePath = $filePath;
+        $this->delimiter = $delimiter;
+
+        return $this;
+    }
+
+    /**
+     * set encoding conversion options
+     *
+     * @param string $fromEncoding
+     * @param string $toEncoding
+     * @return $this
+     */
+    public function setEncoding($fromEncoding = 'UTF-8', $toEncoding = 'UTF-8')
+    {
+        $this->fromEncoding = $fromEncoding;
+        $this->toEncoding = $toEncoding;
+
+        return $this;
+    }
+
+
+    /**
+     * return file as array of objects
+     *
+     * @param array $objectFields Object field names
+     * @return $this
+     */
+    public function toObject(array $objectFields = [])
+    {
+        $this->objectFields = $objectFields;
+
+        return $this;
+    }
+
+
+    /**
+     * format a given line by key using a callable
+     * callable must have one param $val and return $val
+     *
+     * @param $key
+     * @param callable $callable
+     * @return $this
+     */
+    public function format($key, callable $callable)
+    {
+        foreach ((array)$key as $k) {
+            $this->formatters[$k][] = $callable;
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * set a callable to be called on each line to filter lines to retrieve
+     * callable must have one param $line and return a boolean
+     *
+     * @param callable $callable
+     * @return $this
+     */
+    public function filter(callable $callable)
+    {
+        $this->filter = $callable;
+
+        return $this;
+    }
+
+
+    /**
+     * set a callable to be called on each line
+     * callable must have one param $line and return $line
+     *
+     * @param callable $callable
+     * @return $this
+     */
+    public function each(callable $callable)
+    {
+        $this->each = $callable;
+
+        return $this;
+    }
+
+
+    /**
+     * set grouping rules to return file contents grouped in an associative array
+     * callable must have one param $line and return string (grouping key)
+     *
+     * @param callable $callable
+     * @return $this
+     */
+    public function group(callable $callable)
+    {
+        $this->group = $callable;
+
+        return $this;
+    }
+
+
+    /**
+     * Parse file and return file contents
+     *
+     * @return array
+     */
+    public function parse()
+    {
+        $lines = [];
+        $file = null;
+
+        try {
+            $lines = [];
+
+            $file = fopen($this->filePath, "r");
+            while (($line = fgets($file)) !== false) {
+
+                if ($this->delimiter !== null) {
+                    $line = explode($this->delimiter, $line);
+                    // change encoding
+                    if ($this->fromEncoding !== null && $this->toEncoding !== null) {
+                        $line = array_map(function ($val) {
+                            return iconv($this->fromEncoding, $this->toEncoding, $val);
+                        }, $line);
+                    }
+                } else {
+                    $line = iconv($this->fromEncoding, $this->toEncoding, $line);
+                }
+
+                // transform lines to object?
+                if ($this->objectFields !== null) {
+                    $line = (object)array_combine($this->objectFields, $line);
+                }
+
+                // execute callable for each line
+                if (is_callable($this->each)) {
+                    $func = $this->each;
+                    $line = $func($line);
+                }
+
+                // execute callable to filter line
+                if (is_callable($this->filter)) {
+                    $func = $this->filter;
+                    if (!(boolean)$func($line)) continue;
+                }
+
+                foreach ($this->formatters as $key => $callables) {
+                    foreach ($callables as $callable) {
+                        if (is_object($line)) {
+                            $line->{$key} = $callable($line->{$key});
+                        } else {
+                            $line[$key] = $callable($line[$key]);
+                        }
+                    }
+                }
+
+                if (is_callable($this->group)) {
+                    $func = $this->group;
+                    $lines[$func($line)][] = $line;
+                } else {
+                    $lines[] = $line;
+                }
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        } finally {
+            if ($file !== null) $file = null;
+        }
+
+        return $lines;
+    }
+
+}
